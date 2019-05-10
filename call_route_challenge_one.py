0b1fab106618314e4595c2c9193efdd01c1a4daa
SarinSwift <sarinyaswift@gmail.com>
2019-05-10 12:07:04 -0700

    new file

--- call_route_challenge_one.py
+++ call_route_challenge_one.py
@@ -1,7 +1,7 @@
 '''hit command + f in the phone number file
-  type in the number you're looking for
-  while number is not found:
-      if a router number is found:
-          return that router number and it's attached price
-      hit backspace on number
+   type in the number you're looking for
+   while number is not found:
+       if a router number is found:
+           return that router number and it's attached price
+       hit backspace on number
 '''
