5c61fc46e5e7debf9010a5ea7e4dd26cc38245ba
SarinSwift <sarinyaswift@gmail.com>
2019-05-16 12:57:57 -0700

    added binary search for second solution completed

--- call_route_challenge_two.py
+++ call_route_challenge_two.py
@@ -1,96 +1,94 @@
 # Routing project challenge 2
 
-# crappy solution
 
-# def read_file(filename):
-#     return [line.strip() for line in open(filename)]
+def turn_to_array_tuple(file_name):
+    '''turns a file into the number and price tuple
+    ex. [(8130, '0.68'), (86153, '0.32')]
+    '''
+    router_price = []
+    f = open(file_name)
+    for line in f:
+        line = line.strip().split(',')      # '[[+3330027], [0.39]]'
+        number = line[0]
+        price = line[1]
+        router_price.append((int(number), price))
+    f.close()
 
-# def create_route_dict(routers):
-#     dict = {}
-#
-#     for string in routers:
-#
-#         substr_price = string[(len(string) - 4): len(string)]
-#         substr_router = string[0: len(string) - 5]
-#         dict[substr_router] = substr_price
-#
-#     return dict
-#
-# def find_longest_prefix(number, dict):
-#     longest_prefix = ""
-#
-    # for router in dict.keys():
-    #     if router == number[0: len(router)]:
-    #         if len(longest_prefix) < len(router):
-    #             longest_prefix = router
-    # return longest_prefix
-#
-# def return_price(string, dict):
-#     if string == '':
-#         return None
-#     return dict[string]
-#
-# result_arr = []
-# routers = read_file('route-costs-106000.txt')
-# numbers = read_file('phone-numbers-1000.txt')
-# router_dict = create_route_dict(routers)
-#
-# for number in numbers:
-#     router = find_longest_prefix(number, router_dict)
-#     price = return_price(router, router_dict)
-#     result = (router, price)
-#     result_arr.append(result)
+    router_price.sort()
 
-# good solution
-class Router(object):
+    return router_price
 
-    def __init__(self):
+def turn_to_numbers(filename):
+    '''takes in a text file and returns an array of strings
+    ex. [15124156620, 14152345678, ...]
+    '''
+    numbers_list = []
+    f = open(filename)
+    for line in f:
+        line = line.strip()
+        numbers_list.append(int(line))
+    f.close()
+    return numbers_list
 
-        self.shortest_router = 100
-        self.longest_router = 0
+def binary_search_recursive(array, item, left=None, right=None):
+    '''returns the price in the router array of tuples, if the router number is the prefix of our number
+    ex. returns either None, or 0.05 which is the price of the found router number
+    '''
+    if right == None:
+        left = 0
+        right = len(array) - 1
+    elif left > right:
+        return None
+    middleIndex = (left + right) // 2
+    middleVal = array[middleIndex][0]       # middleVal is the first item in the tuple (router number)
+    if middleVal == item:
+        return array[middleIndex][1]        # returning the price of the matched router number
+    if middleVal < item:                    # need to search in the right side of array
+        left = middleIndex + 1
+    elif middleVal > item:                  # need to search in the left side of array
+        right = middleIndex - 1
 
-    def read_router_file(self, filename):
-        router_dict = {}
-        f = open(filename)
-        for line in f:
-            line = line.strip().split(',')
-            router_dict[line[0]] = line[1]
-            if self.shortest_router > len(line[0]):
-                self.shortest_router = len(line[0])
-            if self.longest_router < len(line[0]):
-                self.longest_router = len(line[0])
+    return binary_search_recursive(array, item, left, right)
 
-        f.close()
-        return router_dict
 
-    def read_phone_file(self, filename):
-        return [line.strip() for line in open(filename)]
+def second_solution():
+    '''Implementing binary search
+    currently returns an array of [number, costToCall]
+    ex. [['+14237515032', '0.05'], ['+14815535238', 0], ['+14235244135', '0.05']]
+    '''
 
-    def get_longest_prefix(self, dict, number):
-        if len(number) > self.longest_router:
-            chop = len(number) - self.longest_router
-            number = number[:-chop]
+    # Create an array of all our phone numbers
+    # Create an array of tuples from the router costs
+    list_numbers = turn_to_numbers('phone-numbers-1000.txt')            # [15124156620, ...]
+    array_tuples = turn_to_array_tuple('route-costs-106000.txt')        # [(86153, '0.84'), ...]
 
-        while True:
-            if len(number) < self.shortest_router:
+    # todo: create text file as follows: +8615345522316,0.84
+    #                                   +8130345522316,0.68
+    #                                   +4491878473600,0.48
+    #                                   +4452312234601, 0
+
+    final_array = []
+
+    # Loop throuhg all the numbers we want to find and search for the price using the binary method
+    for num in list_numbers:
+        curr_number = num                # 15124156620
+        while str(curr_number) != '':
+            searched_num = binary_search_recursive(array_tuples, curr_number)
+
+            if searched_num == None:
+                # remove the last digit in the number and search on it as the new number
+                cut_int = str(curr_number)[:-1]
+                if cut_int == '':
+                    final_array.append([('+'+str(num)), 0])
+                    break
+                curr_number = int(cut_int)
+            else:
+                final_array.append([('+'+str(num)), searched_num])
                 break
 
-            if number in dict:
-                return dict[number]
-            else:
-                number = number[:-1]
+    return final_array
 
-# recursion
-# set initial phone # length to longest router
-# chop off phone # digits until length < shortest_router
-def main():
-
-    router = Router()
-    router_dict = router.read_router_file('route-costs-1000000.txt')
-    phone_list = router.read_phone_file('phone-numbers-3.txt')
-    price = router.get_longest_prefix(router_dict, '+4444349844894896754')
-    print(price)
 
 
 if __name__ == '__main__':
-    main()
+    print(second_solution())
